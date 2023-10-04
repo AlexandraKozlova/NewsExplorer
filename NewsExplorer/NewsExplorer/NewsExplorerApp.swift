c3b7a2295766848884d426f860e6b12342975723
Aleksandra Kozlova <aleksandrahankevic2@gmail.com>
2023-10-04 14:07:07 +0300

    initial commit

---
+++ NewsExplorer/NewsExplorer/NewsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsExplorerApp.swift
+//  NewsExplorer
+//
+//  Created by Aleksandra on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
